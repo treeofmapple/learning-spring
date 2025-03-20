9d6cffc27e1bcb0433a9d2173dd96182d8504677
Samuel Pereira <samuelsantana778@gmail.com>
2025-03-20 14:25:10 -0300

    minor changes

---
+++ space_management/src/main/java/com/tom/first/management/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.tom.first.management.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@Configuration
+@OpenAPIDefinition(
+	    info = @Info(
+	        title = "",
+	        description = "",
+	        version = "v1",
+	        contact = @Contact(
+	            name = "Pogeku",
+	            url = "https://"
+	        )
+	    ),
+	    servers = {
+	        @Server(url = "http://localhost:8000", description = "Development Server")
+	    }
+	)
+public class SwaggerConfig {
+	
+}
